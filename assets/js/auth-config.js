7e93062d8aa9e9410bd6026e98fcad5e40d09b1f
Flavavisio <flavio.a.rosa87@gmail.com>
2026-08-30 12:53:39 +0100

    Criar configuração partilhada de autenticação

---
+++ assets/js/auth-config.js
@@ -0,0 +1,5 @@
+window.TG_AUTH_CONFIG = Object.freeze({
+  supabaseUrl: 'https://rfniufasivfnrqfntzog.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmbml1ZmFzaXZmbnJxZm50em9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NzI2MjcsImV4cCI6MjA5NzQ0ODYyN30.1hJ7JBpCKo-Wp93HM5skXVnYk3ghyC7mo0rubbvCXyI',
+  appUrl: 'app.html'
+});
